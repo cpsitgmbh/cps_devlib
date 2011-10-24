ffed5a493ae47b7fb6ae3cd1870519d38c617bbc
Nicole Cordes <cordes@cps-it.de>
2011-10-24 16:26:05 +0200

    Defined functions as "public static" and reformatted code

--- lib/class.tx_cpsdevlib_extmgm.php
+++ lib/class.tx_cpsdevlib_extmgm.php
@@ -1,44 +1,44 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2011 Nicole Cordes <cordes@cps-it.de>
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*  A copy is found in the textfile GPL.txt and important notices to the license
-*  from the author is found in LICENSE.txt distributed with these scripts.
-*
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *
+ *  (c) 2011 Nicole Cordes <cordes@cps-it.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 class tx_cpsdevlib_extmgm {
 
 	/**
-	*	Include a CSS file in frontend or backend
-	*
-	*	@param	string				$cssFile: Path to CSS file
-	*	@param	string				$cssName: File depending name to avoid duplicate insert
-	*	@param	string				$rel: Parameter "rel" for generated link-tag
-	*	@param	string				$media: Parameter "media" for generated link-tag
-	*	@param	string				$title: Parameter "title" for generated link-tag
-	*	@return	boolean				Returns true when successfully inserted
-	*
-	*/
-	function addCssFile($cssFile, $cssName = '', $rel = 'stylesheet', $media = 'all', $title = '') {
+	 * Include a CSS file in frontend or backend
+	 *
+	 * @param string $cssFile: Path to CSS file
+	 * @param string $cssName: File depending name to avoid duplicate insert
+	 * @param string $rel: Parameter "rel" for generated link-tag
+	 * @param string $media: Parameter "media" for generated link-tag
+	 * @param string $title: Parameter "title" for generated link-tag
+	 * @return boolean Returns true when successfully inserted
+	 *
+	 */
+	public static function addCssFile($cssFile, $cssName = '', $rel = 'stylesheet', $media = 'all', $title = '') {
 
 		if (!$cssName) $cssName = $cssFile;
 		$cssLink = '<link rel="' . htmlspecialchars($rel) . '" type="text/css" href="' . htmlspecialchars($cssFile) . '" media="' . htmlspecialchars($media) . '"' . ($title ? ' title="' . htmlspecialchars($title) . '"' : '') . self::getEndingSlash() . '>';
@@ -77,14 +77,14 @@ class tx_cpsdevlib_extmgm {
 	}
 
 	/**
-	*	Include inline CSS in frontend or backend
-	*
-	*	@param	string				$cssCode: Code to insert
-	*	@param	string				$cssName: Code depending name to avoid duplicate insert
-	*	@return	boolean				Returns true when successfully inserted
-	*
-	*/
-	function addCssInline($cssCode, $cssName = '') {
+	 * Include inline CSS in frontend or backend
+	 *
+	 * @param string $cssCode: Code to insert
+	 * @param string $cssName: Code depending name to avoid duplicate insert
+	 * @return boolean Returns true when successfully inserted
+	 *
+	 */
+	public static function addCssInline($cssCode, $cssName = '') {
 
 		if (!$cssName) $cssName = substr(md5($cssCode), 0, 30);
 		$cssStyle = '<style type="text/css">' . LF . '/*<![CDATA[*/' . LF . '<!-- ' . LF . $cssCode . LF . '-->' . LF . '/*]]>*/' . LF . '</style>';
@@ -123,15 +123,15 @@ class tx_cpsdevlib_extmgm {
 	}
 
 	/**
-	*	Include a JavaScript file in frontend or backend
-	*
-	*	@param	string				$jsFile: Path to JavaScript file
-	*	@param	string				$jsName: File depending name to avoid duplicate insert
-	*	@param	string				$type: Parameter "type" for generated script-tag
-	*	@return	boolean				Returns true when successfully inserted
-	*
-	*/
-	function addJavascriptFile($jsFile, $jsName = '', $type = 'text/javascript') {
+	 * Include a JavaScript file in frontend or backend
+	 *
+	 * @param string $jsFile: Path to JavaScript file
+	 * @param string $jsName: File depending name to avoid duplicate insert
+	 * @param string $type: Parameter "type" for generated script-tag
+	 * @return boolean Returns true when successfully inserted
+	 *
+	 */
+	public static function addJavascriptFile($jsFile, $jsName = '', $type = 'text/javascript') {
 
 		if (!$jsName) $jsName = $jsFile;
 		$jsScript = '<script src="' . htmlspecialchars($jsFile) . '" type="' . htmlspecialchars($type) . '"></script>';
@@ -170,15 +170,15 @@ class tx_cpsdevlib_extmgm {
 	}
 
 	/**
-	*	Include inline JavaScript in frontend or backend
-	*
-	*	@param	string				$jsCode: Code to insert
-	*	@param	string				$jsName: Code depending name to avoid duplicate insert
-	*	@param	string				$type: Parameter "type" for generated script-tag
-	*	@return	boolean				Returns true when successfully inserted
-	*
-	*/
-	function addJavascriptInline($jsCode, $jsName = '', $type = 'text/javascript') {
+	 * Include inline JavaScript in frontend or backend
+	 *
+	 * @param string $jsCode: Code to insert
+	 * @param string $jsName: Code depending name to avoid duplicate insert
+	 * @param string $type: Parameter "type" for generated script-tag
+	 * @return boolean Returns true when successfully inserted
+	 *
+	 */
+	public static function addJavascriptInline($jsCode, $jsName = '', $type = 'text/javascript') {
 
 		if (!$jsName) $jsName = substr(md5($jsCode), 0, 30);
 		$jsScript = '<script type="' . htmlspecialchars($type) . '">' . LF . $jsCode . LF . '</script>';
@@ -217,12 +217,12 @@ class tx_cpsdevlib_extmgm {
 	}
 
 	/**
-	*	Return ending slash for xhtml document doctypes (and html5)
-	*
-	*	@return	string				Either ending slash or none
-	*
-	*/
-	function getEndingSlash() {
+	 * Return ending slash for xhtml document doctypes (and html5)
+	 *
+	 * @return string Either ending slash or none
+	 *
+	 */
+	public static function getEndingSlash() {
 		// Array with all xhtml doctypes
 		$xhtmlArray = array(
 			'html_5',
@@ -252,4 +252,5 @@ class tx_cpsdevlib_extmgm {
 		return $result;
 	}
 }
+
 ?>
